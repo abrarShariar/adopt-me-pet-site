fcf17808d3cdb79c58bf4b87d2fc0072fc2544f7
Abrar Shariar <shariarabrar@gmail.com>
2020-10-04 12:07:27 +0900

    Add custom hook

--- src/SearchParam.js
+++ src/SearchParam.js
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropDown";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  //   const [animal, setAnimal] = useState("dog");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdowm, updateBreed] = useDropdown("Breed", "", breeds);
 
   return (
     <div className="search-params">
+      <h1>{location}</h1>
       <form>
         <label htmlFor="location">
-          Location
           <input
             id="location"
             value={location}
@@ -15,6 +21,26 @@ const SearchParams = () => {
             onChange={(e) => setLocation(e.target.value)}
           />
         </label>
+
+        <AnimalDropdown />
+        <BreedDropdowm />
+
+        <label htmlFor="animal">Animal: </label>
+
+        <select
+          id="animal"
+          value={animal}
+          onChange={(e) => setAnimal(e.target.value)}
+          onBlur={(e) => setAnimal(e.target.value)}
+        >
+          <option>All</option>
+          {ANIMALS.map((item) => (
+            <option value={item} key={item}>
+              {item}
+            </option>
+          ))}
+        </select>
+
         <button>Submit</button>
       </form>
     </div>
